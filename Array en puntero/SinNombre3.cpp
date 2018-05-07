6cc158748ad63e3bcc8a4768e2acf8590de749d6
Horckun <horckun@gmail.com>
2018-05-07 17:36:04 +0200

    07/08/18
    Nuevo ejercicio hecho

---
+++ Array en puntero/SinNombre3.cpp
@@ -0,0 +1,19 @@
+// test
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	int array[5];
+	int *p;
+	
+	p = array;
+	
+	cout << array << endl;
+	cout << p << endl;
+	
+	getch();
+	return 0;
+}
