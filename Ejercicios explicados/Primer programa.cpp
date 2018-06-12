d0a15887bbf33b6805f34ab6a7233a4d34865404
Horckun <horckun@gmail.com>
2018-06-12 19:34:50 +0200

    12/06/18
    Nuevo ejercicio comentado

---
+++ Ejercicios explicados/Primer programa.cpp
@@ -0,0 +1,18 @@
+// Primer programa
+
+// Librerias
+#include <iostream>
+using namespace std;
+
+// Funcion principal
+int main(void) {
+	
+	// Mensaje en pantalla
+	cout << "Mi primer programa." << endl;
+	
+	// Hacemos una pausa
+	cout << "Presiona Intro para finalizar...";
+	cin.get();
+	
+	return 0;
+}
