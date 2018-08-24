c38088c884fe7a8f848ee0247486bf1fc608d7cc
Horckun <horckun@gmail.com>
2018-08-24 20:59:29 +0200

    24/08/18
    COUT

---
+++ Ejercicios explicados/013 - Cout.cpp
@@ -0,0 +1,15 @@
+// COUT
+
+#include <iostream>
+
+using namespace std;
+
+// FUNCIÓN PRINCIPAL
+int main() {
+	cout << "Hola mundo!" << endl;
+	
+	// HACEMOS PAUSA
+	cout << "Presiona Intro para terminar...";
+	cin.get();
+	return 0;
+}
