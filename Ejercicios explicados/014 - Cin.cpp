fcca1c57ee846e8a6915f4462791d6e25b912d73
Horckun <horckun@gmail.com>
2018-08-29 20:22:28 +0200

    29/08/18
    CIN

---
+++ Ejercicios explicados/014 - Cin.cpp
@@ -0,0 +1,22 @@
+// CIN
+
+// LIBRERÍA
+#include <iostream>
+
+using namespace std;
+
+// FUNCIÓN FPRINCIPAL
+int main() {
+	int x;
+	
+	// MOSTRAMOS EN PANTALLA
+	cout << "Introduce un numero: ";
+	
+	// SOLICITAMOS VALOR
+	cin >> x;
+	
+	// MOSTRAMOS EL VALOR POR PANTALLA
+	cout << "El valor introducido es " << x << endl;
+	
+	return 0;
+}
