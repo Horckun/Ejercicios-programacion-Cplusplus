6002705caf7857d28c82d31c8aa2aecae40099b9
Horckun <horckun@gmail.com>
2018-09-12 13:56:21 +0200

    12/09/18
    FOR

---
+++ Ejercicios explicados/020 - For.cpp
@@ -0,0 +1,20 @@
+// FOR
+
+// LIBRERÍA
+#include <iostream>
+
+using namespace std;
+
+// FUNCIÓN PRINCIPAL
+int main() {
+	int i;
+	
+	for(i = 0; i < 11; i++) {
+		cout << i << " ";
+	}
+	cout << endl;
+	
+	cout << "Presiona Intro para terminar...";
+	cin.get();
+	return 0;
+}
