1ccff6225c2af781abff41f73aa2b81b115af184
Horckun <horckun@gmail.com>
2018-10-04 12:59:51 +0200

    04/10/18
    Cadena de carácteres

---
+++ Ejercicios explicados/023 - Cadena de carácteres.cpp
@@ -0,0 +1,20 @@
+// Cadena de carácteres
+
+// Librería
+#include <iostream>
+
+using namespace std;
+
+// Función principal
+int main() {
+	int i;
+	char palabra[10];
+	
+	cout << "Introduce la primera letra de la palabra: ";
+	cin >> palabra;
+	
+	cout << palabra;
+	
+	// Cierre
+	return 0;
+}
