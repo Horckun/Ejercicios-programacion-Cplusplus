86bcd68037957457a8bdd2b92df064baf7e9b3dd
Horckun <horckun@gmail.com>
2018-11-06 18:35:43 +0100

    Update
    Cerrar archivo

---
+++ Ejercicios explicados/042 - Cerrar archivo.cpp
@@ -0,0 +1,26 @@
+// Cerrar archivo
+
+// Librería
+#include <iostream>
+
+using namespace std;
+
+// Función principal
+int main() {
+	// Archivo *variable
+	FILE *archivo;
+	// Variable = abrir archivo
+	archivo = fopen("archivo.txt","rt");
+	// Comprobar si se abre
+	if(archivo == NULL) {
+		cout << "No se ha podido abrir el archivo." << endl;
+	}
+	
+	// Cerrar archivo
+	fclose(archivo);
+	
+	// Cierre
+	cout << "Pulsa Intro para finalizar...";
+	cin.get();
+	return 0;
+}
